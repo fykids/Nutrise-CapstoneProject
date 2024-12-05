9c571d6fcde85ab70e26403661b20364699e4408
fyosu <josuahabeahan2016@gmail.com>
2024-12-05 21:33:02 +0700

    update

--- app/src/main/java/com/capstone/nutrise/SplashScreenActivity.kt
+++ app/src/main/java/com/capstone/nutrise/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.capstone.nutrise
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.capstone.nutrise.databinding.ActivitySplashScreenBinding
 import com.capstone.nutrise.view.onboarding.OnBoardingActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySplashScreenBinding
 
