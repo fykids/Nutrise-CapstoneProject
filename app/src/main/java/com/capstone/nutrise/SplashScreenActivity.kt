aa0448a127bec279b7bef214c66723e0929aa7b9
fyosu <josuahabeahan2016@gmail.com>
2024-12-05 21:12:20 +0700

    update

--- app/src/main/java/com/capstone/nutrise/SplashScreenActivity.kt
+++ app/src/main/java/com/capstone/nutrise/SplashScreenActivity.kt
@@ -14,20 +14,17 @@ import com.capstone.nutrise.view.onboarding.OnBoardingActivity
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySplashScreenBinding
 
-    override fun onCreate(savedInstanceState : Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    class SplashScreenActivity : AppCompatActivity() {
+        private lateinit var binding : ActivitySplashScreenBinding
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, OnBoardingActivity::class.java))
-            finish()
-        }, 3000)
-    }
+        override fun onCreate(savedInstanceState : Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this, OnBoardingActivity::class.java))
+                finish()
+            }, 3000)
+        }
 }
