e89790596ee00e88148f2f6519411b4bc211b904
fyosu <josuahabeahan2016@gmail.com>
2024-12-05 21:16:45 +0700

    update

--- app/src/main/java/com/capstone/nutrise/SplashScreenActivity.kt
+++ app/src/main/java/com/capstone/nutrise/SplashScreenActivity.kt
@@ -4,27 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.capstone.nutrise.databinding.ActivitySplashScreenBinding
 import com.capstone.nutrise.view.onboarding.OnBoardingActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySplashScreenBinding
 
-    class SplashScreenActivity : AppCompatActivity() {
-        private lateinit var binding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        override fun onCreate(savedInstanceState : Bundle?) {
-            super.onCreate(savedInstanceState)
-            binding = ActivitySplashScreenBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-
-            Handler(Looper.getMainLooper()).postDelayed({
-                startActivity(Intent(this, OnBoardingActivity::class.java))
-                finish()
-            }, 3000)
-        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, OnBoardingActivity::class.java))
+            finish()
+        }, 3000)
+    }
 }
