431fe014cdeecd094944f665d93c185cbf436711
Riyan-GitHub <moh.nurfitriyan@gmail.com>
2024-12-05 21:41:19 +0700

    percobaan

--- app/src/main/java/com/capstone/nutrise/SplashScreenActivity.kt
+++ app/src/main/java/com/capstone/nutrise/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.nutrise.databinding.ActivitySplashScreenBinding
 import com.capstone.nutrise.view.onboarding.OnBoardingActivity
@@ -14,6 +15,7 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
